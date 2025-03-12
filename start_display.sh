e8a7e0388774f63b33817d5b7e0784849a5aea4a
Richard Kemp <richardtkemp@gmail.com>
2025-03-12 15:51:11 +0000

    Specify python3

--- start_display.sh
+++ start_display.sh
@@ -3,4 +3,4 @@
 DIR="$(dirname "$(readlink -f "$0")")"
 mkdir -p $DIR/logs
 
-$(which python) $DIR/display.py | tee -a $DIR/logs/$(date +%Y%m%d%H%M%S).log
+python3 $DIR/display.py | tee -a $DIR/logs/$(date +%Y%m%d%H%M%S).log
