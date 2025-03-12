4176c2e243fdaff07d7b92d4899710d812d583a8
Richard Kemp <richardtkemp@gmail.com>
2025-03-12 16:10:06 +0000

    Add file for epdconfig

--- start_display.sh
+++ start_display.sh
@@ -1,10 +1,13 @@
 #!/usr/bin/env bash
 
 DIR="$(dirname "$(readlink -f "$0")")"
-mkdir -p $DIR/logs
+mkdir -p "$DIR/logs" "$DIR/libs"
 
-lib="$DIR/libs/epd13in3E.py"
-if [[ ! -f "$lib" ]] ; then
-	curl -o "$lib" https://raw.githubusercontent.com/waveshareteam/e-Paper/refs/heads/master/E-paper_Separate_Program/13.3inch_e-Paper_E/RaspberryPi/python/lib/epd13in3E.py
-fi
+for l in epd13in3E.py epdconfig.py ; do
+	libfile="$DIR/libs/$l"
+        if [[ ! -f "$libfile" ]] ; then
+		curl -o "$libfile" "https://raw.githubusercontent.com/waveshareteam/e-Paper/refs/heads/master/E-paper_Separate_Program/13.3inch_e-Paper_E/RaspberryPi/python/lib/$l"
+	fi
+done
+
 python3 $DIR/display.py | tee -a $DIR/logs/$(date +%Y%m%d%H%M%S).log
