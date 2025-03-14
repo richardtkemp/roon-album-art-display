2ddfa1db49b399aa2ccb2fc4dda44548b47475a7
Richard Kemp <richardtkemp@gmail.com>
2025-03-14 18:10:37 +0000

    Save last image key, not the image Add rotation options - 90 180 270 Don't run API thread in daemon mode else python will quit (when using eink so tkinter is not running it's own loop on main) Interrupt e-ink rendering if a new image comes in Add libs into this repo directly rather than copying in from another repo Add better logging to e-ink libs Improve fetching for now playing info Add track title to display logging

--- start_display.sh
+++ start_display.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
+set -e
 DIR="$(dirname "$(readlink -f "$0")")"
-mkdir -p "$DIR/logs" "$DIR/libs"
-
-# TODO fetch epaper repo
-mkdir libs
-cp ../e-Paper/E-paper_Separate_Program/13.3inch_e-Paper_E/RaspberryPi/python/lib/* libs/
 
 python3 $DIR/display.py | tee -a $DIR/logs/$(date +%Y%m%d%H%M%S).log
